51039645595dcb4be42d4c41e7d4e9b10b3a9ed3
Jonathan Anjos <jonathan.d.anjos@accenture.com>
2020-06-12 16:22:23 -0300

    refactor: setting token secret on environment variables

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,7 +1,7 @@
 const JWT = require("jsonwebtoken");
 const UserController = require("../controllers/user");
 
-const secret = "secret";
+const secret = process.env.TOKEN_SECRET;
 
 const validate = async (decoded) => {
   const user = await UserController.findById(decoded.sub);
