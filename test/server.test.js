5e289ee3bc8cb68ce1be244c08e8565c4e2cdf7a
Jonathan Anjos <jonathan.d.anjos@accenture.com>
2020-06-21 18:26:23 -0300

    feature: basic user route tests

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-multi-assign */
 const Lab = require("@hapi/lab");
 const { expect } = require("@hapi/code");
+const jwtUtils = require("../utils/jwt");
 
 const {
   afterEach,
@@ -31,6 +32,36 @@ describe("GET /users", () => {
         url: "/",
       });
       expect(res.statusCode).to.equal(404);
+    });
+  });
+
+  test("with invalid authentication token", () => {
+    it("responds with 401", async () => {
+      const res = await server.inject({
+        method: "get",
+        url: "/",
+        Auth: "invalid.token",
+      });
+      expect(res.statusCode).to.equal(401);
+    });
+  });
+
+  test("with valid authentication token", () => {
+    it("responds with 201", async () => {
+      const res = await server.inject({
+        method: "get",
+        url: "/",
+        Auth: jwtUtils.generateJWT(
+          {
+            email: "email@email.com",
+          },
+          {
+            subject: "01",
+            expiresIn: "1m",
+          }
+        ),
+      });
+      expect(res.statusCode).to.equal(201);
     });
   });
 });
